85f8c3bc07dfdb63f2c287c662de06bad8a9c87d
Alexis <alexis.prunier@securitymadein.lu>
2021-03-02 17:54:43 +0100

    Remove unused css/js + Remove service worker

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { unregister } from './registerServiceWorker';
 import { CookiesProvider } from 'react-cookie';
-
-//unregister();
 
 ReactDOM.render(
 	<CookiesProvider>
