365d8fa241b3a756afb660becef8ec51759bbc3e
Alexis <alexis.prunier@securitymadein.lu>
2021-05-17 16:37:59 +0200

    Redirect user to external website if article has a "link"

--- src/component/item/Article.jsx
+++ src/component/item/Article.jsx
@@ -12,34 +12,52 @@ export default class Article extends Component {
 		};
 	}
 
+	getBoxContent() {
+		return <div className="Article card">
+			<div className="card-img-wrapper">
+				{this.props.info.image !== null && this.props.info.image !== undefined
+					? <img
+						className="card-img-top"
+						src={getApiURL() + "public/get_image/" + this.props.info.image}
+						alt="Card image cap"/>
+					: <NoImage
+						height={200}
+					/>
+				}
+				<div className="card-date">{this.props.info.publication_date}</div>
+				<div className="card-type">{this.props.info.type}</div>
+			</div>
+			<div className="card-body">
+				<h5 className="card-title">{this.props.info.title}</h5>
+				<p className="card-text">{this.props.info.abstract}</p>
+				{this.props.info.link !== null
+					&& this.props.info.link !== undefined
+					? <button
+						className={"blue-background"}
+					>
+						<i className="fas fa-arrow-alt-circle-right"/> Open website
+					</button>
+					: <button
+						className={"blue-background"}
+					>
+						<i className="fas fa-arrow-alt-circle-right"/> Read article
+					</button>}
+			</div>
+		</div>;
+	}
+
 	render() {
-		return (
-			<Link to={"/news/" + this.props.info.handle} className="Article-link">
-				<div className="Article card">
-					<div className="card-img-wrapper">
-						{this.props.info.image !== null && this.props.info.image !== undefined
-							? <img
-								className="card-img-top"
-								src={getApiURL() + "public/get_image/" + this.props.info.image}
-								alt="Card image cap"/>
-							: <NoImage
-								height={200}
-							/>
-						}
-						<div className="card-date">{this.props.info.publication_date}</div>
-						<div className="card-type">{this.props.info.type}</div>
-					</div>
-					<div className="card-body">
-						<h5 className="card-title">{this.props.info.title}</h5>
-						<p className="card-text">{this.props.info.abstract}</p>
-						<button
-							className={"blue-background"}
-						>
-							<i className="fas fa-arrow-alt-circle-right"/> More info
-						</button>
-					</div>
-				</div>
-			</Link>
-		);
+		return this.props.info.link !== null
+			&& this.props.info.link !== undefined
+			? <a
+				href={this.props.info.link}
+				target={"_blank"}
+				rel="noreferrer"
+				className="Article-link">
+				{this.getBoxContent()}
+			</a>
+			: <Link to={"/news/" + this.props.info.handle} className="Article-link">
+				{this.getBoxContent()}
+			</Link>;
 	}
 }
