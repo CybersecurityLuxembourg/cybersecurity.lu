aeb73c874f0fe34928315f3621d8b4e4a14f24d6
Alexis <alexis.prunier@securitymadein.lu>
2021-09-16 10:26:46 +0200

    Clean footer and add ecosystem menus

--- src/component/bar/Footer.jsx
+++ src/component/bar/Footer.jsx
@@ -18,79 +18,120 @@ export default class Footer extends React.Component {
 				<div className="Footer-content">
 					<div className="row">
 						<div className="col-md-3">
-
-							<div className="Footer-patronage">
-								Under the High Patronage of the <br/><b>Ministry of the Economy</b>
-							</div>
-							<div className="Footer-title">KEY PARTNERS</div>
-							<div className="Footer-alinea">
-								<a
-									href="https://hcpn.gouvernement.lu/en.html"
-									rel="noreferrer"
-									target="_blank"
-									title="HCPN">
-									High Commission for National Protection
-								</a>
-							</div>
-							<br/>
-							<div className="Footer-alinea">
-								<a
-									href="https://securitymadein.lu/"
-									rel="noreferrer"
-									target="_blank"
-									title="SECURITYMADEIN.LU GIE"
-									className="text-capitalize">
-									SECURITYMADEIN.LU
-								</a>
-							</div>
-							<br/>
-							<div className="Footer-alinea">
-								<a
-									href="https://www.luxinnovation.lu/"
-									rel="noreferrer"
-									target="_blank"
-									title="LUXINNOVATION GIE"
-									className="text-capitalize">
-									Luxinnovation
-								</a>
+							<div className="row">
+								<div className="col-md-12">
+									<div className="Footer-patronage">
+										Under the High Patronage of the <br/><b>Ministry of the Economy</b>
+									</div>
+									<br/>
+									<div className="Footer-title">KEY PARTNERS</div>
+									<br/>
+									<div className="Footer-alinea">
+										<a
+											href="https://hcpn.gouvernement.lu/en.html"
+											rel="noreferrer"
+											target="_blank"
+											title="HCPN">
+											High Commission for National Protection
+										</a>
+									</div>
+									<br/>
+									<div className="Footer-alinea">
+										<a
+											href="https://securitymadein.lu/"
+											rel="noreferrer"
+											target="_blank"
+											title="SECURITYMADEIN.LU GIE"
+											className="text-capitalize">
+											SECURITYMADEIN.LU
+										</a>
+									</div>
+									<br/>
+									<div className="Footer-alinea">
+										<a
+											href="https://www.luxinnovation.lu/"
+											rel="noreferrer"
+											target="_blank"
+											title="LUXINNOVATION GIE"
+											className="text-capitalize">
+											Luxinnovation
+										</a>
+									</div>
+								</div>
 							</div>
 						</div>
-						<div className="col-md-1"/>
-						<div className="col-md-2">
+						<div className="col-md-4">
 							<div className="Footer-title">MENU</div>
-							<div>
-								<Link to="/"><b>Home</b></Link>
-							</div>
-							<div>
-								<Link to="/about"><b>About</b></Link>
-							</div>
-							<br/>
-							<div>
-								<Link to="/news">What&apos;s up?</Link>
-							</div>
-							<div>
-								<Link to="/calendar">Where to meet?</Link>
-							</div>
-							<div>
-								<a
-									href={getEcosystemAppURL()}
-									rel="noreferrer"
-									title="Ecosystem">
-									Ecosystem
-								</a>
-							</div>
-							<br/>
-							<div>
-								<a
-									href={getPrivateAppURL()}
-									rel="noreferrer"
-									target="_blank"
-									title="My CYBERLUX">
-									My CYBERLUX
-								</a>
+
+							<div className="row">
+								<div className="col-md-6">
+									<div>
+										<Link to="/">Home</Link>
+									</div>
+									<br/>
+									<div>
+										<Link to="/strategy">Strategy</Link>
+									</div>
+									<br/>
+									<div>
+										<Link to="/news">What&apos;s up?</Link>
+									</div>
+									<div>
+										<Link to="/calendar">Where to meet?</Link>
+									</div>
+									<div>
+										<Link to="/marketplace">Job marketplace</Link>
+									</div>
+									<br/>
+									<div>
+										<Link to="/about">About</Link>
+									</div>
+									<div>
+										<a href={getPrivateAppURL()}>
+											My CYBERLUX
+										</a>
+									</div>
+								</div>
+
+								<div className="col-md-6">
+									<div><b>Ecosystem</b></div>
+									<br/>
+									<div className="Footer-alinea">
+										<a href={getEcosystemAppURL()}>
+											Home
+										</a>
+									</div>
+									<br/>
+									<div className="Footer-alinea">
+										<a href={getEcosystemAppURL() + "privatesector"}>
+											Private sector
+										</a>
+									</div>
+									<div className="Footer-alinea">
+										<a href={getEcosystemAppURL() + "publicsector"}>
+											Public sector
+										</a>
+									</div>
+									<div className="Footer-alinea">
+										<a href={getEcosystemAppURL() + "civilsociety"}>
+											Civil society
+										</a>
+									</div>
+									<br/>
+									<div className="Footer-alinea">
+										<a href={getEcosystemAppURL() + "dashboard"}>
+											Dashboard
+										</a>
+									</div>
+									<div className="Footer-alinea">
+										<a href={getEcosystemAppURL() + "map"}>
+											Map
+										</a>
+									</div>
+								</div>
 							</div>
 						</div>
-						<div className="col-md-4 Footer-contact">
+						<div className="col-md-3 Footer-contact">
 							<div className="Footer-title">CONTACT</div>
 
 							<div>SECURITYMADEIN.LU g.i.e.</div>
