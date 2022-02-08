a63b864a297f80a79bc0c4f2be8c8bd76881fd72
Alexis <alexis.prunier@securitymadein.lu>
2022-02-08 15:45:41 +0100

    Minor change + clean code on home page

--- src/component/PageHome.jsx
+++ src/component/PageHome.jsx
@@ -1,8 +1,5 @@
 import React from "react";
 import "./PageHome.css";
-import { NotificationManager as nm } from "react-notifications";
-import Loading from "./box/Loading.jsx";
-import Message from "./box/Message.jsx";
 import ShadowBoxPcDoctor from "./box/ShadowBoxPcDoctor.jsx";
 import ShadowBoxPureStartup from "./box/ShadowBoxPureStartup.jsx";
 import ShadowBoxEducation from "./box/ShadowBoxEducation.jsx";
@@ -10,108 +7,13 @@ import ShadowBoxCyber4Growth from "./box/ShadowBoxCyber4Growth.jsx";
 import ShadowBoxECSC from "./box/ShadowBoxECSC.jsx";
 import ShadowBoxJobs from "./box/ShadowBoxJobs.jsx";
 import PageHomeLatestNews from "./pagehome/PageHomeLatestNews.jsx";
-import { getRequest } from "../utils/request.jsx";
-import Article from "./item/Article.jsx";
-import { dictToURI } from "../utils/url.jsx";
 
 export default class PageHome extends React.Component {
 	constructor(props) {
 		super(props);
 
 		this.state = {
-			callToActionNews: null,
-			callToActionNewsCompany: null,
 		};
-	}
-
-	componentDidMount() {
-		this.getCallToActionNews();
-	}
-
-	componentDidUpdate(prevProps) {
-		if (prevProps.analytics === null && this.props.analytics !== null) {
-			this.getCallToActionNews();
-		}
-	}
-
-	getCallToActionNews() {
-		if (this.props.analytics
-			&& this.props.analytics.taxonomy_values) {
-			const values = this.props.analytics.taxonomy_values
-				.filter((v) => v.category === "ARTICLE CATEGORY")
-				.filter((v) => v.name === "CALL TO ACTION");
-
-			if (values > 0) {
-				const params = {
-					type: "NEWS",
-					include_tags: "true",
-					taxonomy_values: values.map((v) => v.id).join(","),
-					per_page: 3,
-					page: 1,
-				};
-
-				getRequest.call(this, "public/get_public_articles?" + dictToURI(params), (data) => {
-					this.setState({
-						callToActionNews: data,
-					}, () => {
-						const params2 = {
-							ids: Array.prototype.concat.apply(
-								[],
-								data.items
-									.filter((i) => i.company_tags)
-									.map((i) => i.company_tags),
-							),
-						};
-
-						getRequest.call(this, "public/get_public_companies?" + dictToURI(params2), (data2) => {
-							this.setState({
-								callToActionNewsCompanies: data2,
-							});
-						}, (response) => {
-							nm.warning(response.statusText);
-						}, (error) => {
-							nm.error(error.message);
-						});
-					});
-				}, (response) => {
-					nm.warning(response.statusText);
-				}, (error) => {
-					nm.error(error.message);
-				});
-			} else {
-				this.setState({
-					callToActionNews: { pagination: { total: 0 } },
-				});
-			}
-		}
-	}
-
-	getNewsContent(news) {
-		if (!news) {
-			return <Loading
-				height={150}
-			/>;
-		}
-
-		if (this.props.analytics) {
-			if (news.pagination.total === 0) {
-				return <Message
-					text={"No article found"}
-					height={150}
-				/>;
-			}
-
-			return news.items.map((a) => <div
-				className={"col-md-4"}
-				key={a.id}>
-				<Article
-					info={a}
-					companies={this.state.callToActionNewsCompany}
-				/>
-			</div>);
-		}
-
-		return "";
 	}
 
 	// eslint-disable-next-line class-methods-use-this
