90f645aac094bf5ecd1245be690bed6d67975663
Alexis <alexis.prunier@securitymadein.lu>
2022-03-04 15:12:40 +0100

    Remove services on best practice page

--- src/component/pagebestpractices/BestPracticesPage.jsx
+++ src/component/pagebestpractices/BestPracticesPage.jsx
@@ -2,9 +2,7 @@ import React from "react";
 import "./BestPracticesPage.css";
 import { NotificationManager as nm } from "react-notifications";
 import { getRequest } from "../../utils/request.jsx";
-import Service from "../item/Service.jsx";
 import Tool from "../item/Tool.jsx";
-import DynamicTable from "../table/DynamicTable.jsx";
 import Loading from "../box/Loading.jsx";
 import Message from "../box/Message.jsx";
 import { dictToURI } from "../../utils/url.jsx";
@@ -14,67 +12,18 @@ export default class BestPracticesPage extends React.Component {
 		super(props);
 
 		this.state = {
-			services: null,
 			tools: null,
 		};
 	}
 
 	componentDidMount() {
-		this.fetchServices();
 		this.fetchTools();
 	}
 
 	componentDidUpdate(prevProps) {
 		if ((!prevProps.taxonomyValue && this.props.taxonomyValue)
 			|| (!prevProps.analytics && this.props.analytics)) {
-			this.fetchServices();
 			this.fetchTools();
-		}
-	}
-
-	fetchServices(page) {
-		if (this.props.analytics && this.props.taxonomyValue) {
-			const params = {
-				type: "SERVICE",
-				page: page || 1,
-				per_page: 9,
-				taxonomy_values: this.props.taxonomyValue.id,
-				include_tags: true,
-			};
-
-			getRequest.call(this, "public/get_public_articles?"
-				+ dictToURI(params), (data) => {
-				this.setState({
-					services: data,
-				});
-			}, (response) => {
-				nm.warning(response.statusText);
-			}, (error) => {
-				nm.error(error.message);
-			});
-		}
-	}
-
-	fetchTrainings(page) {
-		if (this.props.analytics && this.props.taxonomyValue) {
-			const params = {
-				type: "SERVICE",
-				page: page || 1,
-				per_page: 9,
-				taxonomy_values: this.props.taxonomyValue.id,
-				include_tags: true,
-			};
-
-			getRequest.call(this, "public/get_public_articles?"
-				+ dictToURI(params), (data) => {
-				this.setState({
-					services: data,
-				});
-			}, (response) => {
-				nm.warning(response.statusText);
-			}, (error) => {
-				nm.error(error.message);
-			});
 		}
 	}
 
@@ -177,40 +126,6 @@ export default class BestPracticesPage extends React.Component {
 							height={300}
 						/>
 					}
-
-					<div className="col-md-12">
-						<h2>Services</h2>
-					</div>
-
-					<div className="col-md-12">
-						{this.state.services && this.state.services.pagination.total > 0
-							&& <DynamicTable
-								items={this.state.services.items}
-								pagination={this.state.services.pagination}
-								changePage={(page) => this.getServices(page)}
-								buildElement={(a) => <div className="col-md-4">
-									<Service
-										info={a}
-										analytics={this.props.analytics}
-									/>
-								</div>
-								}
-							/>
-						}
-
-						{this.state.services && this.state.services.pagination.total === 0
-							&& <Message
-								text={"No service found"}
-								height={300}
-							/>
-						}
-
-						{!this.state.services
-							&& <Loading
-								height={300}
-							/>
-						}
-					</div>
 				</div>
 			</div>
 		);
