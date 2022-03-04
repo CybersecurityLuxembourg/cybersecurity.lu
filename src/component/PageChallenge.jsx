741de49be4c597a7beac5ca261445e9853b3012b
Alexis <alexis.prunier@securitymadein.lu>
2022-03-04 17:23:25 +0100

    Set content in Challenge page

--- src/component/PageChallenge.jsx
+++ src/component/PageChallenge.jsx
@@ -26,7 +26,44 @@ export default class PageChallenge extends React.Component {
 
 				<div className="row">
 					<div className="col-md-12">
-						CONTENT
+						<h1>Lêtz Cybersecurity Challenge - The national challenge is back!</h1>
+					</div>
+
+					<div className="col-md-12 row-spaced">
+						<h3>
+							Save the date!
+						</h3>
+
+						<h3>
+							Lëtz Cybersecurity Challenge will take place during
+							LuxSkills competition from 04-06 May 2022.
+						</h3>
+					</div>
+
+					<div className="col-md-4 offset-md-4">
+						<img
+							className="PageChallenge-image"
+							src="img/letz-cyberchallenge.jpg"
+							alt="Lêtz Cybersecurity Challenge logo"
+						/>
+					</div>
+
+					<div className="col-md-12 row-spaced">
+						<p>
+							<h4>
+								Take your chance to join the national team that will represent
+								Luxembourg at the European Cybersecurity Challenge in Vienna,
+								13-16 September 2022.
+							</h4>
+						</p>
+
+						<p>
+							<h4>All details to be unveiled soon! Stay tuned…</h4>
+						</p>
+
+						<p>
+							<h4>Any questions so far? Contact <a href="mailto:info@cybersecurity-luxembourg.com">info@cybersecurity-luxembourg.com</a></h4>
+						</p>
 					</div>
 				</div>
 			</div>
