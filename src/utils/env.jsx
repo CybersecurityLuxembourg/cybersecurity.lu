170d1190a9c0a26935f2af219c777f4f26724108
Alexis <alexis.prunier@securitymadein.lu>
2022-03-08 08:50:49 +0100

    Change private app target

--- src/utils/env.jsx
+++ src/utils/env.jsx
@@ -9,7 +9,7 @@ export function getPrivateAppURL() {
 	if (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1" || window.location.hostname === "") {
 		return "http://localhost:3001/";
 	}
-	return "https://my." + window.location.hostname.replace("www.", "") + "/";
+	return "https://community." + window.location.hostname.replace("www.", "") + "/";
 }
 
 export function isInternetExplorer() {
