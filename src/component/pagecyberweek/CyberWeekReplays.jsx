c97200c1337056764a505d5c3a08b1e823080984
Alexis <alexis.prunier@securitymadein.lu>
2022-11-23 15:09:29 +0100

    Update CSWL Replay page

--- src/component/pagecyberweek/CyberWeekReplays.jsx
+++ src/component/pagecyberweek/CyberWeekReplays.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "./CyberWeekReplays.css";
-import Message from "../box/Message.jsx";
 
 export default class CyberWeekReplays extends React.Component {
 	// eslint-disable-next-line class-methods-use-this
@@ -12,21 +11,242 @@ export default class CyberWeekReplays extends React.Component {
 						<h2>Replays</h2>
 					</div>
 
-					<div className="offset-md-3"/>
+					<div className="col-md-12">
+						<h3>Thursday 20.10 morning - Keynotes</h3>
+					</div>
 
-					<div className="col-md-6">
-						<Message
-							text={"Content incoming soon"}
-							height={250}
+					<div className="col-md-12">
+						<h4>A global investor&apos;s view of the cyber
+						sector - Ken Pentimonti, Paladin Capital Group</h4>
+					</div>
+
+					<div className="col-md-12">
+						<iframe
+							title="CSWL 2022 - A global investor's view of the cyber sector - Ken Pentimonti, Paladin Capital Group"
+							width="560"
+							height="315"
+							src="https://peertube.securitymadein.lu/videos/embed/c8d72bf2-1b20-41bb-af36-56a2dc200e58"
+							allowFullScreen=""
+							sandbox="allow-same-origin allow-scripts allow-popups"
+						/>
+					</div>
+
+					<div className="col-md-12">
+						Presentation slides: <a href="https://api.cybersecurity.lu/public/get_public_document/Paladin_CSWL2022.pdf" target="_blank" rel="noreferrer"> Click here</a>
+					</div>
+
+					<div className="col-md-12">
+						<h4>Coordinate Vulnerability Disclosure: An Instrument
+						to improve Cybersecurity Resilience - G. Lenzini, SnT</h4>
+					</div>
+
+					<div className="col-md-12">
+						<iframe
+							title="CSWL2022 - Coordinate Vulnerability Disclosure: An Instrument to improve Cybersecurity Resilience - G. Lenzini, SnT"
+							width="560"
+							height="315"
+							src="https://peertube.securitymadein.lu/videos/embed/bf809a77-4d4d-4bdc-86d9-5c7251973106"
+							allowFullScreen=""
+							sandbox="allow-same-origin allow-scripts allow-popups"
+						/>
+					</div>
+
+					<div className="col-md-12">
+						<h4>Security Metrics That Work - Wim Remes, Damovo</h4>
+					</div>
+
+					<div className="col-md-12">
+						<iframe
+							title="CSWL2022 - Security Metrics That Work - Wim Remes, Damovo"
+							width="560"
+							height="315"
+							src="https://peertube.securitymadein.lu/videos/embed/94c4d1f1-eaba-4a6e-af8b-d83871072f3f"
+							allowFullScreen=""
+							sandbox="allow-same-origin allow-scripts allow-popups"
+						/>
+					</div>
+
+					<div className="col-md-12">
+						Presentation slides: <a href="https://api.cybersecurity.lu/public/get_public_document/Damovo_CSWL2022.pdf" target="_blank" rel="noreferrer"> Click here</a>
+					</div>
+
+					<div className="col-md-12">
+						<h4>How IAM contributes to satisfy the need
+						for Zero Trust - Fujitsu & One Identity</h4>
+					</div>
+
+					<div className="col-md-12">
+						<iframe
+							title="CSWL 2022 - How IAM contributes to satisfy the need for Zero Trust - Fujitsu &amp; One Identity"
+							width="560"
+							height="315"
+							src="https://peertube.securitymadein.lu/videos/embed/ad9b6aa0-19d3-46ef-a905-a1f3606b9503"
+							allowFullScreen=""
+							sandbox="allow-same-origin allow-scripts allow-popups"
+						/>
+					</div>
+
+					<div className="col-md-12">
+						Presentation slides: <a href="https://api.cybersecurity.lu/public/get_public_document/FUJITSU_CSWL2022.pdf" target="_blank" rel="noreferrer"> Click here</a>
+					</div>
+
+					<div className="col-md-12">
+						<h4>DDoS Mitigation : Trends and Challenges in
+						Digital World - Seva Vayner, G-Core Labs</h4>
+					</div>
+
+					<div className="col-md-12">
+						<iframe
+							title="CSWL 2022 - DDoS Mitigation : Trends and Challenges in Digital World - Seva Vayner, G-Core Labs"
+							width="560"
+							height="315"
+							src="https://peertube.securitymadein.lu/videos/embed/4a9bde3e-8fae-41a2-bc04-9db38a1ee5ca"
+							allowFullScreen=""
+							sandbox="allow-same-origin allow-scripts allow-popups"
+						/>
+					</div>
+
+					<div className="col-md-12">
+						Presentation slides: <a href="https://api.cybersecurity.lu/public/get_public_document/Gcore_CSWL2022.pdf" target="_blank" rel="noreferrer"> Click here</a>
+					</div>
+
+					<div className="col-md-12">
+						<h4>On the Frontlines of Fighting Cyber Attacks: Lessons
+						learned from Ukraine - Oleh Derevianko, ISSP</h4>
+					</div>
+
+					<div className="col-md-12">
+						<iframe
+							title="CSWL2022 - On the Frontlines of Fighting Cyber Attacks: Lessons learned from Ukraine - Oleh Derevianko, ISSP"
+							width="560"
+							height="315"
+							src="https://peertube.securitymadein.lu/videos/embed/775a993f-ff7b-4b11-87fe-7b081645f395"
+							allowFullScreen=""
+							sandbox="allow-same-origin allow-scripts allow-popups"
+						/>
+					</div>
+
+					<div className="col-md-12">
+						Presentation slides: <a href="https://api.cybersecurity.lu/public/get_public_document/ISSP_CSWL2022.pdf" target="_blank" rel="noreferrer"> Click here</a>
+					</div>
+
+					<div className="col-md-12">
+						<h3>Thursday 20.10 noon – Cybersecurity Startups Pitching Session</h3>
+					</div>
+
+					<div className="col-md-12">
+						<h4>Introduction panel</h4>
+					</div>
+
+					<div className="col-md-12">
+						<iframe
+							title="CSWL 2022 - Cybersecurity Startups Pitching Session - Introduction Panel"
+							width="560"
+							height="315"
+							src="https://peertube.securitymadein.lu/videos/embed/52538bc8-d175-4ef4-8197-60476cb8615a"
+							allowFullScreen=""
+							sandbox="allow-same-origin allow-scripts allow-popups"
+						/>
+					</div>
+
+					<div className="col-md-12">
+						<h4>Pitching session</h4>
+					</div>
+
+					<div className="col-md-12">
+						<iframe
+							title="CSWL 2022 - Cybersecurity Startups Pitching Session"
+							width="560"
+							height="315"
+							src="https://peertube.securitymadein.lu/videos/embed/0e41d464-38ef-46a9-be35-581dc7a0ac67"
+							allowFullScreen=""
+							sandbox="allow-same-origin allow-scripts allow-popups"
+						/>
+					</div>
+
+					<div className="col-md-12">
+						<h3>Thursday 20.10 afternoon – High-Level Panel Discussions & Foresight</h3>
+					</div>
+
+					<div className="col-md-12">
+						<h4>Welcome Remarks &apos;Towards a common cybersecure
+						future&apos; by Luxembourg Prime Minister, Xavier Bettel</h4>
+					</div>
+
+					<div className="col-md-12">
+						<iframe
+							title="CSWL 2022 - Welcome Remarks 'Towards a common cybersecure future' by Luxembourg Prime Minister, Xavier Bettel"
+							width="560"
+							height="315"
+							src="https://peertube.securitymadein.lu/videos/embed/1081a75b-68fb-4b61-8395-806c10bf1c78"
+							allowFullScreen=""
+							sandbox="allow-same-origin allow-scripts allow-popups"
+						/>
+					</div>
+
+					<div className="col-md-12">
+						<h4>Welcome Remarks - Sabrina Sagramola, Luxembourg
+						Chamber of Commerce</h4>
+					</div>
+
+					<div className="col-md-12">
+						<iframe
+							title="CSWL 2022 - Welcome Remarks - Sabrina Sagramola, Luxembourg Chamber of Commerce"
+							width="560"
+							height="315"
+							src="https://peertube.securitymadein.lu/videos/embed/bd303b39-2efa-4b7d-b678-f645918f1d4b"
+							allowFullScreen=""
+							sandbox="allow-same-origin allow-scripts allow-popups"
+						/>
+					</div>
+
+					<div className="col-md-12">
+						<h4>Global challenges of harmful cyber
+						activities and how to embrace local capacity building</h4>
+					</div>
+
+					<div className="col-md-12">
+						<iframe
+							title="CSWL 2022 - Global challenges of harmful cyber activities and how to embrace local capacity building"
+							width="560"
+							height="315"
+							src="https://peertube.securitymadein.lu/videos/embed/86194a75-0490-4c7f-9d3f-e34c90f56415"
+							allowFullScreen=""
+							sandbox="allow-same-origin allow-scripts allow-popups"
+						/>
+					</div>
+
+					<div className="col-md-12">
+						<h4>Making the European Union a leader
+						in cybersecurity and cyberdefence</h4>
+					</div>
+
+					<div className="col-md-12">
+						<iframe
+							title="CSWL 2022 - Making the European Union a leader in cybersecurity and cyberdefence"
+							width="560"
+							height="315"
+							src="https://peertube.securitymadein.lu/videos/embed/b9c499c4-8b05-4c40-9690-e0fdc0643eb8"
+							allowFullScreen=""
+							sandbox="allow-same-origin allow-scripts allow-popups"
+						/>
+					</div>
+
+					<div className="col-md-12">
+						<h4>Public-private collaboration for an innovative cyber future</h4>
+					</div>
+
+					<div className="col-md-12">
+						<iframe
+							title="CSWL 2022 - Public-private collaboration for an innovative cyber future"
+							width="560"
+							height="315"
+							src="https://peertube.securitymadein.lu/videos/embed/432492f8-87ed-46f1-ac5d-6a6302104fef"
+							allowFullScreen=""
+							sandbox="allow-same-origin allow-scripts allow-popups"
 						/>
 					</div>
 				</div>
-
-				<div className="row row-spaced"/>
-				<div className="row row-spaced"/>
-				<div className="row row-spaced"/>
-				<div className="row row-spaced"/>
-				<div className="row row-spaced"/>
 			</div>
 		);
 	}
